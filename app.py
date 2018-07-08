a0047e94824da32ae9aa19de5aa0c72316cf0a86
Stefan Caraiman <stefanc.caraiman@gmail.com>
2018-07-08 10:47:42 -0700

    Fix broken syntax

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ from flask import Flask,redirect
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world:
+def hello_world():
     return 'Hey, we have Flask in a Docker container!'
 
 @app.route('/example-proxy')
-def proxied_content:
+def proxied_content():
     return redirect("http://www.example.com", code=302)
 
 
 if __name == '__main__':
-	port = int(os.environ.get('PORT', 80))
+    port = int(os.environ.get('PORT', 80))
     app.run(host='0.0.0.0', port=port)
