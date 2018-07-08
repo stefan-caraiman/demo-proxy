3cbb806915f3c5b0902ef0ae6fde3e5582efa23d
Stefan Caraiman <stefanc.caraiman@gmail.com>
2018-07-08 09:58:10 -0700

    Update travis pipeline and add dummy webserver

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask,redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world:
+    return 'Hey, we have Flask in a Docker container!'
+
+@app.route('/example-proxy')
+def proxied_content:
+    return redirect("http://www.example.com", code=302)
+
+
+if __name == '__main__':
+	port = int(os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', port=port)
