e7ee93c84382dff02f7859844b2337cd14baae22
Stefan Caraiman <stefanc.caraiman@gmail.com>
2018-07-10 22:44:42 +0300

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name="demo-proxy",
-    version="0.2.0",
+    version="0.2.1",
     description="A demo proxy",
     long_description=open("README.md").read(),
     author="Stefan Caraiman",
